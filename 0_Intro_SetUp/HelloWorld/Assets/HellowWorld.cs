c1a441a8b82201f562f9b2c2fde2f210ce4a808b
Tom Lloyd <tomlloyd1992@gmail.com>
2019-09-22 18:54:23 +0100

    Intro Setup Completed

---
+++ 0_Intro_SetUp/HelloWorld/Assets/HellowWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellowWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello Tom Lloyd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
